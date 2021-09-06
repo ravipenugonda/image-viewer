73eacb5a4ffbec6e2e9ab9c034d6d94c4c4ee92a
Ravi Penugonda <Ravi@Santhis-MacBook-Pro.local>
2021-09-06 12:24:51 -0400

    initial project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <div>Image Viewer</div>
+      </header>
+      <main>
+        <div>Images Data</div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
